a65efe9a499a6b02143cad58cf1212c1deb45d01
tingis <victor.tingstrom@gmail.com>
2022-05-12 19:40:00 +0200

    Adds parameter to __init__

--- stochproc/distributions/__init__.py
+++ stochproc/distributions/__init__.py
@@ -3,11 +3,13 @@ from .prior import Prior
 from .joint import JointDistribution
 from .sinh_arcsinh import SinhArcsinhTransform
 from .prior_module import _HasPriorsModule
+from .parameter import PriorBoundParameter
 
 __all__ = [
     "DistributionModule",
     "Prior",
     "JointDistribution",
     "SinhArcsinhTransform",
-    "_HasPriorsModule"
+    "_HasPriorsModule",
+    "PriorBoundParameter"
 ]
