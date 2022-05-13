13de4de532b21c1cb43a9d7f43a3e0654da74ee2
tingis <victor.tingstrom@gmail.com>
2022-05-13 13:45:07 +0200

    Fixes linear

--- stochproc/timeseries/linear.py
+++ stochproc/timeseries/linear.py
@@ -1,23 +1,19 @@
 import torch
 from .affine import AffineProcess
-from ..typing import ArrayType
+from ..typing import ParameterType
 from ..utils import broadcast_all
 
 
-def _f0d(x, a, b, _):
+def _f0d(x, a, b):
     return b + a * x.values
 
 
-def _f1d(x, a, b, _):
-    return _f2d(x, a, b, _)
+def _f1d(x, a, b):
+    return _f2d(x, a, b)
 
 
-def _f2d(x, a, b, _):
+def _f2d(x, a, b):
     return b + a.matmul(x.values.unsqueeze(-1)).squeeze(-1)
-
-
-def _g(x, a, b, s):
-    return s
 
 
 _mapping = {0: _f0d, 1: _f1d, 2: _f2d}
@@ -30,11 +26,12 @@ class LinearModel(AffineProcess):
         .. math::
             X_{t+1} = b + A \\cdot X_t + \\sigma \\epsilon_{t+1}, \n
             X_0 \\sim p(x_0)
+
     where :math:`X_t, b, \\sigma \\in \\mathbb{R}^n`, :math:`\\{\\epsilon_t\\}` some distribution, and
     :math:`A \\in \\mathbb{R}^{n \\times n}`.
     """
 
-    def __init__(self, a: ArrayType, sigma: ArrayType, increment_dist, b: ArrayType = None, **kwargs):
+    def __init__(self, a: ParameterType, sigma: ParameterType, increment_dist, b: ParameterType = None, **kwargs):
         """
         Initializes the ``LinearModel`` class.
 
@@ -55,6 +52,10 @@ class LinearModel(AffineProcess):
 
         initial_dist = kwargs.pop("initial_dist", None)
 
+        def _mean_scale(x, a_, b_, s_):
+            return _mapping[dimension](x, a_, b_), s_
+
         super(LinearModel, self).__init__(
-            (_mapping[dimension], _g), params, initial_dist or increment_dist, increment_dist, **kwargs
+            _mean_scale, parameters=params, initial_dist=initial_dist or increment_dist, increment_dist=increment_dist,
+            **kwargs
         )
