26730705a146c346801a301ea00a6c74df829c0e
tingis <victor.tingstrom@gmail.com>
2022-05-13 14:24:12 +0200

    Adds drift type

--- stochproc/timeseries/typing.py
+++ stochproc/timeseries/typing.py
@@ -6,3 +6,4 @@ from .state import TimeseriesState
 
 MeanScaleFun = Callable[[TimeseriesState, Tuple[torch.Tensor, ...]], Tuple[torch.Tensor, torch.Tensor]]
 DiffusionFunction = Callable[[TimeseriesState, float, Tuple[torch.Tensor, ...]], Distribution]
+Drift = Callable[[TimeseriesState, Tuple[torch.Tensor, ...]], torch.Tensor]
