78076c66ad3587539a78bc45cd77d34942e1a044
tingis <victor.tingstrom@gmail.com>
2022-05-14 19:59:15 +0200

    Adds local linear trend

--- stochproc/timeseries/models/local_linear_trend.py
+++ stochproc/timeseries/models/local_linear_trend.py
@@ -1,12 +1,9 @@
 import torch
 from torch.distributions import Normal
-from numbers import Number
-from pyro.distributions import Delta
-from .random_walk import RandomWalk
 from ..linear import LinearModel
-from ..chained import AffineChainedStochasticProcess
-from ...typing import ArrayType
-from ...distributions import DistributionWrapper, JointDistribution
+from ...typing import ParameterType
+from ...distributions import DistributionModule
+from ...utils import enforce_named_parameter
 
 
 class LocalLinearTrend(LinearModel):
@@ -21,9 +18,8 @@ class LocalLinearTrend(LinearModel):
 
     def __init__(
         self,
-        sigma: ArrayType,
-        initial_mean: ArrayType = torch.zeros(2),
-        initial_scale: ArrayType = torch.ones(2),
+        sigma: ParameterType,
+        initial_mean: ParameterType = torch.zeros(2),
         **kwargs
     ):
         """
@@ -35,54 +31,13 @@ class LocalLinearTrend(LinearModel):
             kwargs: See base.
         """
 
-        increment_dist = DistributionWrapper(
+        sigma = enforce_named_parameter(scale=sigma)[0]
+
+        increment_dist = DistributionModule(
             Normal, loc=torch.zeros(2), scale=torch.ones(2), reinterpreted_batch_ndims=1
         )
 
-        initial_dist = DistributionWrapper(Normal, loc=initial_mean, scale=initial_scale, reinterpreted_batch_ndims=1)
-
+        initial_dist = DistributionModule(Normal, loc=initial_mean, scale=sigma, reinterpreted_batch_ndims=1)
         a = torch.tensor([[1.0, 0.0], [1.0, 1.0]])
 
         super().__init__(a, sigma, increment_dist, initial_dist=initial_dist, **kwargs)
-
-
-def _smooth_dist_builder(loc, l_0, scale, validate_args=False):
-    init_s = Normal(loc, scale, validate_args=validate_args)
-
-    return JointDistribution(init_s, Delta(l_0), validate_args=validate_args)
-
-
-class SmoothLinearTrend(AffineChainedStochasticProcess):
-    """
-    Implements a "smooth trend model", defined as
-        .. math::
-            L_{t+1} = L_t + S_t, \n
-            S_{t+1} = S_t + \\sigma_s V_{t+1},
-
-    see docs of ``LocalLinearTrend`` for more information regarding parameters.
-    """
-
-    def __init__(
-        self,
-        sigma: ArrayType,
-        l_0: Number = 0.0,
-        initial_mean: ArrayType = 0.0,
-        initial_scale: ArrayType = 1.0,
-        **kwargs
-    ):
-        """
-        Initializes the ``SmoothLinearTrend`` class.
-
-        Args:
-            sigma: Corresponds to :math:`\\sigma_s`.
-            l_0: The initial value of the level component.
-            initial_mean: Optional, specifies the initial mean of the slope.
-            kwargs: See base.
-        """
-
-        rw = RandomWalk(sigma, initial_mean, initial_scale, **kwargs)
-        smooth = LinearModel(
-            torch.ones(2), 0.0, DistributionWrapper(Delta, v=0.0), initial_dist=DistributionWrapper(Delta, v=l_0)
-        )
-
-        super(SmoothLinearTrend, self).__init__(rw=rw, smooth=smooth)
--- test/timeseries/custom.py
+++ test/timeseries/custom.py
@@ -8,6 +8,7 @@ def models():
     yield mods.OrnsteinUhlenbeck(0.025, 0.0, 0.05)
     yield mods.Verhulst(0.025, 1.0, 0.05, 0.1, num_steps=10)
     yield mods.RandomWalk(0.05)
+    yield mods.LocalLinearTrend(torch.tensor([0.01, 0.05]))
 
 
 class TestCustomModels(object):
@@ -15,4 +16,4 @@ class TestCustomModels(object):
         for proc in models():
             x = proc.sample_path(SAMPLES)
 
-            assert (x.shape[0] == SAMPLES) and ~torch.isna(x).any()
+            assert (x.shape[0] == SAMPLES) and ~torch.isnan(x).any()
