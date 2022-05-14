d1458e4f7f2d08c9f8f6ee03086b06c516274ef0
tingis <victor.tingstrom@gmail.com>
2022-05-14 19:54:16 +0200

    Adds random walk test

--- test/timeseries/custom.py
+++ test/timeseries/custom.py
@@ -1,11 +1,13 @@
 import pytest as pt
 from stochproc.timeseries import models as mods
+import torch
 from .affine import SAMPLES
 
 
 def models():
     yield mods.OrnsteinUhlenbeck(0.025, 0.0, 0.05)
     yield mods.Verhulst(0.025, 1.0, 0.05, 0.1, num_steps=10)
+    yield mods.RandomWalk(0.05)
 
 
 class TestCustomModels(object):
@@ -13,4 +15,4 @@ class TestCustomModels(object):
         for proc in models():
             x = proc.sample_path(SAMPLES)
 
-            assert x.shape[0] == SAMPLES
+            assert (x.shape[0] == SAMPLES) and ~torch.isna(x).any()
