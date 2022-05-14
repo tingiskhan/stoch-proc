719b27b14d2ec695b62550010e36af01c45f4974
tingis <victor.tingstrom@gmail.com>
2022-05-14 15:27:33 +0200

    Adds named parameter type

--- stochproc/typing.py
+++ stochproc/typing.py
@@ -1,9 +1,28 @@
 from numbers import Number
-from typing import Union, NamedTuple
+from typing import Union
 import torch
 from .distributions import Prior
 
 
 _ParameterType = Union[Number, torch.Tensor, torch.nn.Parameter, Prior]
-NamedParameter = NamedTuple("NamedParameter", name=str, value=_ParameterType)
+
+
+class NamedParameter(object):
+    """
+    Defines a named parameter type.
+    """
+
+    def __init__(self, name: str, value: _ParameterType):
+        """
+        Initializes the ``NamedParameter`` class.
+
+        Args:
+            name: The name of the parameter.
+            value: The value of the parameter
+        """
+
+        self.name = name
+        self.value = torch.tensor(value) if isinstance(value, Number) else value
+
+
 ParameterType = Union[_ParameterType, NamedParameter]
