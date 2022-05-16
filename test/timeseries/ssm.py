45c12b5a051dedd4965c0e6352a0ea392b2203a4
tingis <victor.tingstrom@gmail.com>
2022-05-16 19:39:54 +0200

    Adds test for joint ssm

--- test/timeseries/ssm.py
+++ test/timeseries/ssm.py
@@ -6,9 +6,22 @@ from .affine import SAMPLES
 
 class TestSSM(object):
     def test_ssm(self):
-        rw = ts.models.RandomWalk(torch.tensor([0.05, 0.01]), initial_mean=torch.zeros(2))
+        rw = ts.models.RandomWalk(0.05)
         ssm = ts.LinearGaussianObservations(rw, torch.tensor([1.0, 0.01]))
 
         x, y = ssm.sample_path(SAMPLES)
 
         assert (x.shape[0] == y.shape[0] == SAMPLES)
+
+    def test_joint_ssm(self):
+        loc_1 = ts.models.RandomWalk(0.05)
+        loc_2 = ts.models.RandomWalk(0.025)
+
+        joint = ts.AffineJointStochasticProcess(loc_1=loc_1, loc_2=loc_2)
+
+        ssm = ts.LinearGaussianObservations(joint, torch.eye(2))
+
+        batch_size = torch.Size([10, 10, 2])
+        x, y = ssm.sample_path(SAMPLES, samples=batch_size)
+
+        assert (y.shape == torch.Size([SAMPLES, *batch_size, 2]))
