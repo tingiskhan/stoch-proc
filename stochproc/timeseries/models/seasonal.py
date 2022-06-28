1eab39662334bf682821679b400fcc8e0f39a99d
victor <baj>
2022-06-28 22:24:12 +0200

    Black and stuff

--- stochproc/timeseries/models/seasonal.py
+++ stochproc/timeseries/models/seasonal.py
@@ -9,23 +9,25 @@ class Seasonal(LinearModel):
     r"""
     Defines a seasonal time series model, i.e. in which we have that
         .. math::
-            s_t = -\sum^{k-1}_{j=1} s_{t-j} + \epsilon_t, \: \epsilon_t \sim \mathcal{N}(0, \sigma).
+            s_t = -\sum^{k}_{j=1} s_{t-j} + \epsilon_t, \: \epsilon_t \sim \mathcal{N}(0, \sigma).
     """
 
-    def __init__(self, period: int, sigma: ParameterType):
+    def __init__(self, period: int, sigma: ParameterType, initial_sigma: ParameterType = None):
         """
         Initializes the :class:`Seasonal` model.
 
         Args:
             period: period to use for the seasonal model.
             sigma: standard deviation of the Guassian noise.
+            initial_sigma: optional, initial standard deviation. If ``None`` uses ``sigma``.
         """
 
         mat = torch.eye(period - 1, period)
         mat = torch.cat((-torch.ones((1, period)), mat), dim=0)
 
         inc_dist = DistributionModule(_build_trans_dist, loc=0.0, scale=1.0, lags=period)
-        initial_dist = DistributionModule(_build_init, alpha=0.0, beta=1.0, sigma=sigma, lags=period)
+        initial_dist = DistributionModule(
+            _build_init, alpha=0.0, beta=1.0, sigma=sigma if initial_sigma is None else initial_sigma, lags=period
+        )
 
         super(Seasonal, self).__init__(mat, sigma, increment_dist=inc_dist, initial_dist=initial_dist)
-
--- stochproc/timeseries/ssm.py
+++ stochproc/timeseries/ssm.py
@@ -49,9 +49,7 @@ class StateSpaceModel(StructuralStochasticProcess):
         init_dist = self.build_density(x)
         empty = float("nan") * torch.ones(init_dist.batch_shape + init_dist.event_shape, device=x.values.device)
 
-        return StateSpaceModelState(
-            x=x, y=TimeseriesState(x.time_index, values=empty, event_dim=init_dist.event_shape)
-        )
+        return StateSpaceModelState(x=x, y=TimeseriesState(x.time_index, values=empty, event_dim=init_dist.event_shape))
 
     def initial_sample(self, shape: torch.Size = torch.Size([])) -> StateSpaceModelState:
         x_state = self.hidden.initial_sample(shape)
