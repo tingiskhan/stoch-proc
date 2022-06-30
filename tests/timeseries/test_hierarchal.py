8902d12fe7a42e5f54ca30517ce36253226c12f1
victor <baj>
2022-06-30 11:40:21 +0200

    Adds assert, changes parameters to avoid infinite growth

--- tests/timeseries/test_hierarchal.py
+++ tests/timeseries/test_hierarchal.py
@@ -15,10 +15,12 @@ def mean_scale(x, beta, sigma):
 class TestHierarchalProcess(object):
     @pytest.mark.parametrize("batch_shape", BATCH_SHAPES)
     def test_hierarchal_processes(self, batch_shape):
-        sub = ts.models.AR(0.0, 0.99, 0.05)
+        sub = ts.models.AR(0.0, 0.99, 0.001)
 
         inc_dist = dists.DistributionModule(Normal, loc=0.0, scale=1.0)
         main = ts.AffineHierarchalProcess(sub, mean_scale, (0.99, 0.05), initial_dist=inc_dist, increment_dist=inc_dist)
+
+        assert main.event_shape == torch.Size([2])
 
         x = main.sample_states(SAMPLES, batch_shape).get_path()
 
