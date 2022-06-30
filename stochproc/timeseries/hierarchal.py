2c92010c7a3360030d90ed19e557b90356424783
victor <baj>
2022-06-30 11:54:55 +0200

    Boom, a lot more neat

--- stochproc/timeseries/hierarchal.py
+++ stochproc/timeseries/hierarchal.py
@@ -1,50 +1,31 @@
 import torch
 
-from . import JointState
-from .affine import AffineProcess
-from ..distributions import JointDistribution, DistributionModule
+from . import AffineProcess, AffineJointStochasticProcess
 
 
-class AffineHierarchalProcess(AffineProcess):
+class AffineHierarchalProcess(AffineJointStochasticProcess):
     r"""
     Defines a "hierarchal" affine process, by which we mean a process that ... TODO
 
     """
 
-    def __init__(self, sub_process: AffineProcess, mean_scale, parameters, initial_dist, increment_dist, **kwargs):
+    def __init__(self, sub_process: AffineProcess, main_process: AffineProcess):
         """
         Initializes the :class:`AffineHierarchalProcess` class.
 
         Args:
             sub_process: child/sub process.
+            main_process: main process.
         """
 
-        super().__init__(mean_scale, parameters, None, DistributionModule(self._inc_builder), **kwargs)
-        self._initial_dist = initial_dist
-        self._increment_dist = increment_dist
+        super(AffineHierarchalProcess, self).__init__(sub=sub_process, main=main_process)
 
-        self.sub_process = sub_process
-        self._event_shape = self.initial_dist.event_shape
+    def mean_scale(self, x, parameters=None):
+        sub_mean, sub_scale = self.sub_processes["sub"].mean_scale(x["sub"])
+        main_mean, main_scale = self.sub_processes["main"].mean_scale(x)
 
-    @property
-    def initial_dist(self):
-        return JointDistribution(self.sub_process.initial_dist, super(AffineHierarchalProcess, self).initial_dist)
-
-    def _inc_builder(self):
-        return JointDistribution(self.sub_process.increment_dist(), self._increment_dist())
-
-    def initial_sample(self, shape: torch.Size = torch.Size([])) -> JointState:
-        return JointState(
-            sub_state=self.sub_process.initial_sample(shape),
-            main_state=super(AffineHierarchalProcess, self).initial_sample(shape)
-        )
-
-    def mean_scale(self, x: JointState, parameters=None):
-        sub_mean, sub_scale = self.sub_process.mean_scale(x["sub_state"])
-        main_mean, main_scale = super(AffineHierarchalProcess, self).mean_scale(x)
-
-        sub_unsqueeze = x["sub_state"].event_dim.numel() == 1
-        main_unsqueeze = x["main_state"].event_dim.numel() == 1
+        sub_unsqueeze = x["sub"].event_dim.numel() == 1
+        main_unsqueeze = x["main"].event_dim.numel() == 1
 
         mean = (
             sub_mean.unsqueeze(-1) if sub_unsqueeze else sub_mean,
--- tests/timeseries/test_hierarchal.py
+++ tests/timeseries/test_hierarchal.py
@@ -9,7 +9,7 @@ from .constants import BATCH_SHAPES
 
 
 def mean_scale(x, beta, sigma):
-    return x["sub_state"].values + beta * x["main_state"].values, sigma
+    return x["sub"].values + beta * x["main"].values, sigma
 
 
 class TestHierarchalProcess(object):
@@ -18,11 +18,13 @@ class TestHierarchalProcess(object):
         sub = ts.models.AR(0.0, 0.99, 0.001)
 
         inc_dist = dists.DistributionModule(Normal, loc=0.0, scale=1.0)
-        main = ts.AffineHierarchalProcess(sub, mean_scale, (0.99, 0.05), initial_dist=inc_dist, increment_dist=inc_dist)
+        main = ts.AffineProcess(mean_scale, (0.99, 0.05), initial_dist=inc_dist, increment_dist=inc_dist)
 
-        assert main.event_shape == torch.Size([2])
+        hieararchal = ts.AffineHierarchalProcess(sub, main)
 
-        x = main.sample_states(SAMPLES, batch_shape).get_path()
+        assert hieararchal.event_shape == torch.Size([2])
 
-        assert x.shape == torch.Size([SAMPLES, *batch_shape, *main.event_shape])
+        x = hieararchal.sample_states(SAMPLES, batch_shape).get_path()
+
+        assert x.shape == torch.Size([SAMPLES, *batch_shape, *hieararchal.event_shape])
 
