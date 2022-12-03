55dd50dd95d61bf94fb298a0ffd4eb18a6175657
Victor <victor.tingstrom@gmail.com>
2022-12-03 19:15:57 +0100

    Adds a path-like object

---
+++ stochproc/path.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Generic, Tuple
+
+from .typing import TArray
+from .state import TimeseriesState
+
+
+@dataclass(frozen=True)
+class CombinedPath(Generic[TArray]):
+    time_indexes: TArray
+    path: TArray
+
+
+class ProcessPath(Generic[TArray]):
+    """
+    Base container object for storing sampled paths from a stochastic process.
+    """
+
+    def __init__(self, states: TimeseriesState[TArray]):
+        """
+        Internal initializer for :class:`ProcessPath`.
+
+        Args:
+            states (TimeseriesState): timeseries states to combine.
+        """
+
+        self.path = sorted(states, key=lambda u: u.time_index)
+    
+    def get_path(self) -> CombinedPath:
+        """
+        Returns the time indexes together with the sampled path.
+
+        Returns:
+            CombinedPath: time indexes together with sampled path.
+        """
+
+        raise NotImplementedError()
+
+    
