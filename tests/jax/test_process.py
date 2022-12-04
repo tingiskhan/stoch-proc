455ec9ce4c719dfbd9dcdf2448774dc552fedbed
Victor <victor.tingstrom@gmail.com>
2022-12-04 19:18:23 +0100

    Fixes tests

--- tests/jax/test_process.py
+++ tests/jax/test_process.py
@@ -1,17 +1,31 @@
 from stochproc.jax import timeseries as ts
 from numpyro.distributions import Normal
 from jax.random import PRNGKey
+import jax.numpy as jnp
+import jax
+import pytest as pt
+
+
+@jax.jit
+def prop(x, a, b):
+    return Normal(a * x.value, b)
 
 
 class TestTimeseries(object):    
-    def test_initialize(self):
+    @pt.mark.parametrize("shape", [(), (10,), (10, 10), (2_000, 500)])
+    def test_initialize_and_sample(self, shape):
         init_dist = Normal()
-        proc = ts.JaxProcess(init_dist)
+        proc = ts.StructuralStochasticProcess(init_dist)
 
         assert proc.event_shape == ()
 
         key = PRNGKey(0)
-        init_values = proc.initial_distribution().sample(key)
+        init_state = proc.initial_state(key, shape=shape)
 
-        assert init_values.shape == ()
+        assert init_state.value.shape == shape
 
+        a = jnp.ones(shape)
+        b = jnp.ones(shape)
+
+        j = prop(init_state, a, b)
+        print()
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,4 +1,4 @@
-from stochproc.state import TimeseriesState
+from stochproc.state import _TimeseriesState
 import jax.numpy as jnp
 import pytest as pt
 import torch
@@ -6,15 +6,15 @@ import torch
 JAX_ARRAY = jnp.ones(10)
 TORCH_ARRAY = torch.ones(10)
 
-class TestState(object):
+class TestState(object): 
     @pt.mark.parametrize("array", [JAX_ARRAY, lambda: JAX_ARRAY])
     def test_jax(self, array):
-        state = TimeseriesState(jnp.array(0), array, ())
+        state = _TimeseriesState(jnp.array(0), array, ())
 
-        assert (state.values == JAX_ARRAY).all()
+        assert (state.value == JAX_ARRAY).all()
 
     @pt.mark.parametrize("array", [TORCH_ARRAY, lambda: TORCH_ARRAY])
     def test_torch(self, array):
-        state = TimeseriesState(torch.tensor(0), array, ())
+        state = _TimeseriesState(torch.tensor(0), array, ())
 
-        assert (state.values == TORCH_ARRAY).all()
+        assert (state.value == TORCH_ARRAY).all()
