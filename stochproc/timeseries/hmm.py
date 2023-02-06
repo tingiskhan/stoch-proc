52762a54a40a43071c73d5a1566774ca81fc2459
Victor <victor.tingstrom@gmail.com>
2023-02-06 19:43:30 +0100

    Fixes HMM

--- stochproc/timeseries/hmm.py
+++ stochproc/timeseries/hmm.py
@@ -8,23 +8,31 @@ from .stochastic_process import StructuralStochasticProcess
 
 def _parameter_transform(*parameters):
     return parameters[0]
+    
 
+def _find_initial_probabilities(probs: torch.Tensor) -> torch.Tensor:
+    ones = torch.ones(probs.shape[:-1], device=probs.device).unsqueeze(-2)
+    eye = torch.eye(probs.shape[-1], device=probs.device)
 
-def _initial_kernel(init_probabilities):
-    return Multinomial(probs=init_probabilities)
+    p = torch.cat((eye - probs.transpose(-2, -1), ones), dim=-2)
+
+    transpose = p.transpose(-2, -1)
+    inverse = torch.linalg.pinv(transpose @ p)
+
+    return (inverse @ transpose)[..., -1]
 
 
 class HiddenMarkovModel(StructuralStochasticProcess):
     """
-    Implements a discrete `Hidden Markov Model`_.
+    Implements a discrete `Hidden Markov Model`_. More resources from `Oxford`.
 
     .. _`Hidden Markov Model`: https://en.wikipedia.org/wiki/Hidden_Markov_model
+    .. _`Oxford`: https://oxfordre.com/economics/display/10.1093/acrefore/9780190625979.001.0001/acrefore-9780190625979-e-174;jsessionid=01A52B578F4523C66A4DCCA27ACB1DE7
     """
 
     def __init__(
         self,
-        transition_matrix: ParameterType,
-        initial_probabilities: ParameterType,
+        parameters: Sequence[ParameterType],
         parameter_transform: Callable[[Sequence[torch.Tensor]], Sequence[torch.Tensor]] = _parameter_transform,
     ):
         """
@@ -33,23 +41,37 @@ class HiddenMarkovModel(StructuralStochasticProcess):
         Args:
             transition_matrix (ParameterType): transition probabilities.
             initial_probabilities (ParameterType): initial probabilities.
-            parameter_transform (Callable[[Sequence[torch.Tensor]], Sequence[torch.Tensor]]): function for transforming parameters.
+            parameter_transform (Callable[[Sequence[torch.Tensor]], Sequence[torch.Tensor]]): function for transforming parameters. Defaults to returning the first parameter as is.
         """
+
+        transition_matrix = _parameter_transform(*parameters)
 
         if transition_matrix.shape[-1] != transition_matrix.shape[-2]:
             raise Exception("Transition matrix is not rectangular!")
 
-        if transition_matrix.shape[-1] != initial_probabilities.shape[-1]:
-            raise Exception("Initial probabilities is not congruent with transition!")
+        if (transition_matrix.sum(dim=-1) != 1.0).all():
+            raise Exception("Not a proper transition matrix!")
+
+        self._parameter_transform = parameter_transform
 
         super().__init__(
-            self._hmm_kernel, (transition_matrix,), _initial_kernel, initial_parameters=(initial_probabilities,)
+            self._hmm_kernel, parameters, self._initial_hmm_kernel, initial_parameters=()
         )
-        self._parameter_transform = parameter_transform
+        
+    @property
+    def initial_probabilities(self) -> torch.Tensor:
+        """
+        Returns the initial probabilities.
+        """
+
+        return _find_initial_probabilities(self._parameter_transform(*self.parameters))
 
     def expand(self, batch_shape):
         new_parameters = self._expand_parameters(batch_shape)
-        return HiddenMarkovModel(*new_parameters["parameters"], *new_parameters["initial_parameters"])
+        return HiddenMarkovModel(new_parameters["parameters"])
+
+    def _initial_hmm_kernel(self, *_):
+        return Multinomial(probs=self.initial_probabilities)
 
     def _hmm_kernel(self, x, *parameters: torch.Tensor):
         probs = self._parameter_transform(*parameters)
--- tests/timeseries/test_hmm.py
+++ tests/timeseries/test_hmm.py
@@ -10,9 +10,9 @@ from .constants import BATCH_SHAPES
 class TestHMM(object):
     @pt.mark.parametrize("batch_shape", BATCH_SHAPES)
     def test_hmm(self, batch_shape):
-        probs = torch.tensor([[0.75, 0.25], [0.1, 0.9]])
+        probs = torch.tensor([[0.35, 0.65], [0.1, 0.9]])
 
-        hmm = HiddenMarkovModel(probs, probs[0])
+        hmm = HiddenMarkovModel((probs,))
         samples = hmm.expand(batch_shape).sample_states(SAMPLES).get_path()
 
         assert samples.shape == torch.Size([SAMPLES]) + batch_shape + hmm.event_shape
