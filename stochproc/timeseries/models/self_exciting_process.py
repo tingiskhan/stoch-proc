dd228151168225e8c69f82638f08b5c2e78ecb24
andreanero <andrea.veronese@fairmat.com>
2023-03-26 11:57:09 +0200

    trying different initial kernels for lambda process. WIP00

--- stochproc/timeseries/models/self_exciting_process.py
+++ stochproc/timeseries/models/self_exciting_process.py
@@ -12,7 +12,7 @@ from ..state import TimeseriesState
 def _initial_kernel(alpha, xi, eta, de):
     exp_j2 = de.p * 2.0 * de.rho_plus.pow(-2.0) + (1.0 - de.p) * 2.0 * de.rho_minus.pow(-2.0)
 
-    std_lambda = exp_j2.sqrt() * eta
+    std_lambda = exp_j2.sqrt() * eta.sqrt()
     # dist_ = TransformedDistribution(Normal(xi, std_lambda), t.AbsTransform())
     dist_ = TransformedDistribution(Normal(xi, std_lambda), t.AbsTransform())
 
